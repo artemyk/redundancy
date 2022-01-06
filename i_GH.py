0b0650253d1acb482c982083efdf0bf5db365f87
Artemy Kolchinsky <artemyk@gmail.com>
2022-01-05 20:16:26 -0700

    Update

--- i_GH.py
+++ i_GH.py
@@ -1,40 +1,61 @@
 # -*- coding: utf-8 -*-
-from ppl import Variable, Constraint_System
-from utils import get_best_solution, mi
+r"""
+Finding I_\cap^GH, as defined in Griffith and Ho (2015)
+  (there it is called I_alpha)
+
+Given a joint distribution p_{Y,X_1,..,X_n}, I_\cap^GH can be 
+written as the solution to the following optimization problem:
+  R = max_{s_{Q|X_1,...,X_n,Y}} I_s(Y;Q)
+                       s.t. H(Q|X_i) = H(Q|X_i, Y) ∀i
+This can in turn be re-written as:
+  R = max_{s_{Q|X_1,...,X_n,Y}} I_s(Y;Q)
+       s.t. ∀i,q,x_i,y  s(q|x_i) = s(q|x_i, y)
+
+Note that this is optimization problem involes a maximization of 
+a convex function, subject to a system of linear constraints.  This 
+system of linear constraints defines a convex polytope, and the maximum 
+of the function will lie at one of the vertices of the polytope.  We 
+proceed by finding these vertices, iterating over them, and finding 
+the vertex with the largest value of I(Y;Q)
+
+Note that, as currently implemented, this can be very slow. Note also that 
+we have not derived a bound on the necessary cardinality for Q (though this 
+can likely be done, using a similar technique as the bound for Istar)
+"""
+
+from scipy.special import entr
+import convex_maximization
 import numpy as np
 
-# Finding I_\cap^GH, as defined in Griffith and Ho (2015)
-#   (there it is called I_alpha)
-
-# Given a joint distribution p_{Y,X_1,..,X_n}, I_\cap^GH can be 
-# written as the solution to the following optimization problem:
-#   R = max_{s_{Q|X_1,...,X_n,Y}} I_s(Y;Q)
-#                        s.t. H(Q|X_i) = H(Q|X_i, Y) ∀i
-# This can in turn be re-written as:
-#   R = max_{s_{Q|X_1,...,X_n,Y}} I_s(Y;Q)
-#        s.t. ∀i,q,x_i,y  s(q|x_i) = s(q|x_i, y)
-# 
-# Note that this is optimization problem involes a maximization of 
-# a convex function, subject to a system of linear constraints.  This 
-# system of linear constraints defines a convex polytope, and the maximum 
-# of the function will lie at one of the vertices of the polytope.  We 
-# proceed by finding these vertices, iterating over them, and finding 
-# the vertex with the largest value of I(Y;Q)
-
-# Note that, as currently implemented, this can be *very* slow. Note also that 
-# we have not derived a bound on the necessary cardinality for Q (though this 
-# can be done, using a similar technique as the bound for Istar)
 
 
 def get_I_GH(raw_pjoint, n_q, eps=1e-8):
-    # mult is a multiplier that is used to turn floating point numbers into integers,
-    # (since ppl only works with rationals)
+    """
+    Parameters
+    ----------
+    raw_pjoint: dit distribution
+        joint distribution object from dit, where the last random
+        variable is the target Y, and the others are the sources X_1 ,..., X_n
+
+    eps: float
+        Rounding error. We must round our conditional probability distributions
+        to rationals (the library ppl requires rationals)
+
+    Returns 
+    -------
+    optimum_value : float
+        Redundancy value
+    sol : dict
+        Solution information, in terms of joint distribution p(Q,Y) and 
+        conditional distributions p(Q|X_i)
+    """    
 
     pjoint = raw_pjoint.copy()
     
     target_rvndx = len(pjoint.rvs) - 1
-    
-    if len(pjoint.marginal([target_rvndx,])) == 1:
+    pY     = pjoint.marginal([target_rvndx,])
+
+    if len(pY) == 1:
         # Trivial case where target only has a single output
         return 0, {}
     
@@ -42,19 +63,19 @@ def get_I_GH(raw_pjoint, n_q, eps=1e-8):
     rv_names = ['X%d'%(i+1) for i in range(n_sources)] + ['Y',]
     pjoint.set_rv_names(rv_names)
 
-    def k(d):
-        return "".join(d[rv] for rv in rv_names)
-
     
     n_y  = len(pjoint.alphabet[-1])
     variables = {}
     for o in pjoint.sample_space():
         for q in range(n_q):
-            variables[(q, o)] = Variable(len(variables))  # s(q|x_1,...,x_n,y')
+            variables[(q, o)] = len(variables)  # s(q|x_1,...,x_n,y')
 
 
 
     # We represent our feasible set in terms of equalities and inequalities
+    A_eq  , b_eq   = [], []  # linear constraints Ax  = b
+    A_ineq, b_ineq = [], []  # linear constraints Ax <= b
+
 
     # The following puts in place the basic system of constraints:
     # each probability should be non-negative, and the conditional 
@@ -62,15 +83,22 @@ def get_I_GH(raw_pjoint, n_q, eps=1e-8):
 
     # all variables should be positive
 
-    cs = Constraint_System()
+    num_vars = len(variables)
+    for k,v in variables.items():
+        z = np.zeros(num_vars, dtype='int')
+        z[v] = -1
+        A_ineq.append(z)
+        b_ineq.append(0)
 
-    for v in variables.values():
-        cs.insert(v >= 0)
 
     for ndx, o in enumerate(pjoint.sample_space()):
-        # for each x1, .., x_n, y : \sum_q p(q|x1,...,x_n,y) = 1 
-        cs.insert(sum(variables[(q,o)] for q in range(n_q)) == 1)
-        
+        # for each x1, .., x_n, y : \sum_q s(q|x1,...,x_n,y) = 1 
+        sum_to_one = np.zeros(num_vars, dtype='int')
+        for q in range(n_q):
+            sum_to_one[variables[(q,o)]]=1
+        A_eq.append(sum_to_one)
+        b_eq.append(1)
+
         # We now specify the remaining constraints:
         # ∀ q,x_1,y : 
         #   s(q|x_1) = s(q|x_1, y)
@@ -78,6 +106,8 @@ def get_I_GH(raw_pjoint, n_q, eps=1e-8):
         #   Σ_{x_2,...x_n,y'} s(q|x_1,...,x_n,y') p(x_2,y'|x_1) = \sum_{x_2,...x_n} s(q|x_1,...,x_n,y) p(x_2,...,x_n|x_1,y) ,
         # and similarly for the other sources
         
+        def k(d):
+            return "".join(d[rv] for rv in rv_names)
         
         for source in pjoint.get_rv_names()[:-1]:
             pX , pOtherYgX = pjoint.condition_on([source], rv_mode='names')
@@ -86,12 +116,15 @@ def get_I_GH(raw_pjoint, n_q, eps=1e-8):
 
             # ∀i,q,x_i,y s(q|x_i) = s(q|x_i, y)
             
-            for q in range(n_q):
+            for q in range(n_q-1):
+                # we can drop constraints on final q due to conservation of probability
                 for xy in pXYoutcomes:
                     if pXY[xy] == 0.:
                         continue
 
-                    sumL , sumR  = 0, 0
+                    lhs = np.zeros(num_vars, dtype='int')
+                    rhs = np.zeros(num_vars, dtype='int')
+
                     normL, normR = 0, 0
                     x,y      = xy
                     ix       = pX._outcomes_index[x]        # condition on X
@@ -104,7 +137,7 @@ def get_I_GH(raw_pjoint, n_q, eps=1e-8):
                         valdict.update(dict(zip(otherrvs, othervals)))
 
                         normL += int(p/eps)
-                        sumL  += int(p/eps) * variables[q,k(valdict)]
+                        lhs[variables[q,k(valdict)]] += int(p/eps)
 
                     ix       = pXY._outcomes_index[xy]         # condition on X
                     # calculate s(q|x_1,y) = Σ s(q|x_1,...x_n, y) p(x_2,...x_n|x_1,y)
@@ -115,23 +148,44 @@ def get_I_GH(raw_pjoint, n_q, eps=1e-8):
                         valdict.update(dict(zip(otherrvs, othervals)))
 
                         normR += int(p/eps)
-                        sumR  += int(p/eps) * variables[q,k(valdict)]
+                        rhs[variables[q,k(valdict)]] += int(p/eps)
 
-
-                    cs.insert(sumL*normR - sumR*normL == 0)
+                    A_eq.append(lhs*normR - rhs*normL)
+                    b_eq.append(0)
                 
 
-    sol_mx = np.zeros( (n_q*n_y, len(variables)) )
+    mul_mx = np.zeros( (len(variables), n_q*n_y) )
     for (q,o), k in variables.items():
-        y=int(o[-1])
-        sol_mx[q*n_y + y, k.id()] = pjoint[o]
-                             
-    def get_solution_val(x, full=False):
-        pQY = sol_mx.dot(x).reshape(n_q, n_y)
-        mival = mi(pQY)
-        return {'raw':np.array(x), 'pQY':pQY}, mival
-    
-    return get_best_solution(cs, get_solution_val)
+        y= o[-1]
+        y_ix = pY._outcomes_index[y]
+        mul_mx[k, q*n_y + y_ix] = pjoint[o]
 
+    probs_y = np.zeros(n_y)
+    for y in pjoint.alphabet[target_rvndx]:
+        probs_y[pY._outcomes_index[y]] = pY[y]
+
+    H_Y = entr(probs_y).sum()
+    ln2 = np.log(2)
+    def objective(x):
+        # Map solution vector x to joint distribution over Q and Y
+        pQY = x.dot(mul_mx).reshape((n_q,n_y))
+        probs_q = pQY.sum(axis=1) + 1e-18
+        H_YgQ = entr(pQY/probs_q[:,None]).sum(axis=1).dot(probs_q)
+        return (H_Y-H_YgQ)/ln2
+
+    # The following uses ppl to turn our system of linear inequalities into a 
+    # set of extreme points of the corresponding polytope. It then calls 
+    # get_solution_val on each extreme point
+    x_opt, v_opt = convex_maximization.maximize_convex_function(
+        f=objective,
+        A_eq=np.array(A_eq, dtype='int'), 
+        b_eq=np.array(b_eq, dtype='int'), 
+        A_ineq=np.array(A_ineq, dtype='int'), 
+        b_ineq=np.array(b_ineq, dtype='int'))
+
+    sol = {}
+    sol['p(Q,Y)'] = x_opt.dot(mul_mx).reshape((n_q,n_y))
+
+    return v_opt, sol   
 
 
--- istar.py
+++ istar.py
@@ -1,55 +1,67 @@
 # -*- coding: utf-8 -*-
-import ppl
-from utils import get_best_solution, mi
+r"""
+Compute our proposed redundancy measure, $I_\cap^\star$.
+
+Given a joint distribution p_{Y,X_1,X_2}, our redundancy measure is 
+the solution to the following optimization problem:
+
+R = min_{s_{Q|Y}} I_s(Y;Q) 
+             s.t. s_{Q|Y} ⪯ p_{X_i|Y} ∀i
+
+This can in turn be re-written as:
+ R = min_{s_{Q|Y},s_{Q|X_1}, .., s_{Q|X_n}} I_s(Y;Q)
+     s.t. ∀i,q,y : Σ_{x_i} s(q|x_i) p(x_i,y) = s(q|y)p(y)
+
+Note that this is optimization problem involes a maximization of 
+a convex function subject to a system of linear constraints.  This 
+system of linear constraints defines a convex polytope, and the maximum 
+of the function will lie at one of the vertices of the polytope.  We 
+proceed by finding these vertices, iterating over them, and finding 
+the vertex with the largest value of I(Y;Q).
+"""
 
 import numpy as np
-
-# Compute our proposed redundancy measure, $I_\cap^\star$.
-
-# Given a joint distribution p_{Y,X_1,X_2}, our redundancy measure is 
-# the solution to the following optimization problem:
-# 
-# R = min_{s_{Q|Y}} I_s(Y;Q) 
-#              s.t. s_{Q|Y} ⪯ p_{X_i|Y} ∀i
-# 
-# This can in turn be re-written as:
-#  R = min_{s_{Q|Y},s_{Q|X_1}, .., s_{Q|X_n}} I_s(Y;Q)
-#      s.t. ∀i,q,y : Σ_{x_i} s(q|x_i) p(x_i,y) = s(q|y)p(y)
-# 
-# Note that this is optimization problem involes a maximization of 
-# a convex function subject to a system of linear constraints.  This 
-# system of linear constraints defines a convex polytope, and the maximum 
-# of the function will lie at one of the vertices of the polytope.  We 
-# proceed by finding these vertices, iterating over them, and finding 
-# the vertex with the largest value of I(Y;Q).
-# We use the computational geometry library 'ppl'.
-
+from scipy.special import entr
+import convex_maximization
 
 
 def get_Istar(raw_pjoint, eps=1e-8):
-    # raw_pjoint is a joint distribution object from dit, where the last random
-    #    variable is the target Y, and the others are the sources X_1 ,..., X_n
-    # eps is needed because we round our conditional probability distributions
-    #    to rationals (the library ppl requires rationals)
-    
-    pjoint = raw_pjoint.copy()
+    """
+    Parameters
+    ----------
+    raw_pjoint: dit distribution
+        joint distribution object from dit, where the last random
+        variable is the target Y, and the others are the sources X_1 ,..., X_n
+
+    eps: float
+        Rounding error. We must round our conditional probability distributions
+        to rationals (the library ppl requires rationals)
+
+    Returns 
+    -------
+    optimum_value : float
+        Redundancy value
+    sol : dict
+        Solution information, in terms of joint distribution p(Q,Y) and 
+        conditional distributions p(Q|X_i)
+    """
+
+    pjoint       = raw_pjoint.copy()
     
     target_rvndx = len(pjoint.rvs) - 1
-    pY = pjoint.marginal([target_rvndx,])
+    pY           = pjoint.marginal([target_rvndx,], rv_mode='indices')
+    n_y          = len(pY)
     
-    if len(pY) == 1:
+    if n_y <= 1:
         # Trivial case where target has a single outcome, redundancy has to be 0
         return 0, {}
-    
-    # Set up a ppl object that will encode a system of linear inequalities
-    cs = ppl.Constraint_System()
     
     # variablesQgiven holds ppl variables that represent conditional probability
     # values of s(Q=q|X_i=x_i) for different sources i, as well as s(Q=q|Y=y)
     # for the target Y (recall that Q is our redundancy random variable)
     variablesQgiven = {} 
 
-    num_vars        = 0  # counter for tracking how many ppl variables we've created
+    var_ix        = 0  # counter for tracking how many variables we've created
     
 
     # Calculate the maximum number of outcomes we will require for Q
@@ -69,21 +81,35 @@ def get_Istar(raw_pjoint, eps=1e-8):
         for v in pjoint.alphabet[rvndx]:
             sum_to_one = 0 
             for q in range(n_q):
-                cvar        = ppl.Variable(num_vars)
-                num_vars   += 1
-                
                 # represents s(Q=q|X_rvndx=v) if rvndx != target_rvndx
                 #        and s(Q=q|Y=v)       if rvndx == target_rvndx
-                variablesQgiven[rvndx][(q, v)] = cvar 
+                variablesQgiven[rvndx][(q, v)] = var_ix 
+                var_ix += 1
+    
+    num_vars = var_ix 
 
-                cs.insert(cvar >= 0) # Enforces non-negativity
+    A_eq  , b_eq   = [], []  # linear constraints Ax =b
+    A_ineq, b_ineq = [], []  # linear constraints Ax<=b
 
-                sum_to_one += cvar
+    for rvndx, rv in enumerate(pjoint.rvs):
+        for v in pjoint.alphabet[rvndx]:
+            sum_to_one = np.zeros(num_vars, dtype='int')
+            for q in range(n_q):
+                var_ix = variablesQgiven[rvndx][(q, v)]
+
+                # Non-negative constraint on each variable
+                z = np.zeros(num_vars)
+                z[var_ix] = -1
+                A_ineq.append(z) 
+                b_ineq.append(0)
+
+                sum_to_one[var_ix] = 1
 
             if rvndx != target_rvndx:
             	# Linear constraint that enforces Σ_q s(Q=q|X_rvndx=v) = 1
-                cs.insert(sum_to_one == 1)
-                
+                A_eq.append(sum_to_one)
+                b_eq.append(1)
+
     # Now we add the constraint:
     #    ∀i,q,y : Σ_{x_i} s(q|x_i) p(x_i,y) = s(q|y)p(y)
     for rvndx, rv in enumerate(pjoint.rvs):
@@ -94,49 +120,60 @@ def get_Istar(raw_pjoint, eps=1e-8):
         pYSource = pjoint.marginal([rvndx,target_rvndx,], rv_mode='indices')
         for q in range(n_q):
             for y in pjoint.alphabet[target_rvndx]:
-                constraint = 0
+                z = np.zeros(num_vars, dtype='int')
                 cur_mult   = 0.  # multiplier to make everything rational, and make rounded values add up to 1
                 for x in pjoint.alphabet[rvndx]:
                 	# We divide by eps and round, and then multiply by cur_mult,
                 	# to make everything rational
                     pXY        = int( pYSource[pYSource._outcome_ctor((x,y))] / eps )
                     cur_mult   += pXY
-                    constraint += pXY * variablesQgiven[rvndx][(q, x)]
-                cs.insert(constraint == cur_mult*variablesQgiven[target_rvndx][(q, y)])
+                    z[variablesQgiven[rvndx][(q, x)]] = pXY 
+                z[variablesQgiven[target_rvndx][(q, y)]] = -cur_mult
+                A_eq.append(z)
+                b_eq.append(0)
 
     # Define a matrix sol_mx that allows for fast mapping from solution vector 
     # returned by ppl (i.e., a particular extreme point of our polytope) to a 
     # joint distribution over Q and Y
-    n_y    = len(pY)
-    sol_mx = np.zeros((num_vars, n_q*n_y))
+    mul_mx = np.zeros((num_vars, n_q*n_y))
+    y_ixs  = {}
     for (q,y), k in variablesQgiven[target_rvndx].items():
-        y_ix = pY._outcomes_index[y]
-        sol_mx[k.id(), q*n_y + y_ix] += pY[y]
+        if y not in y_ixs: 
+            y_ixs[y] = len(y_ixs)
+        mul_mx[k, q*n_y + y_ixs[y]] += pY[y]
 
-    def get_solution_val(x, full=False):
-    	# Computes the mutual information for a particular solution vector 
-    	# return by ppl (in practice, this will be some extreme point of our polytope)
-    	# x is the solution vector
-    	# full=True returns full solution information.Slower and only used once best solution is found
+    H_Y = entr([pY[y] for y in pjoint.alphabet[target_rvndx]]).sum()
+    ln2 = np.log(2)
 
+    def objective(x):
         # Map solution vector x to joint distribution over Q and Y
-        sol = { 'pQY' : x.dot(sol_mx).reshape((n_q,n_y)) }
+        pQY = x.dot(mul_mx).reshape((n_q,n_y))
+        probs_q = pQY.sum(axis=1) + 1e-18
+        H_YgQ = entr(pQY/probs_q[:,None]).sum(axis=1).dot(probs_q)
+        return (H_Y - H_YgQ)/ln2
 
-        if full:
-	        # Compute conditional distributions of Q given each source X_i
-            # This is not necessary to figure out which extreme point is best
-            for rvndx in range(len(pjoint.rvs)-1):
-                pX = pjoint.marginal([rvndx,])
-                sol[rvndx] = np.zeros( (n_q, len(pX.alphabet[0]) ) )
-                for (q,v), k in variablesQgiven[rvndx].items():
-                    v_ix = pX._outcomes_index[v]
-                    sol[rvndx][q,v_ix] = x[k.id()]
-
-        # Return solution information and mutual information I(Q;Y)
-        return sol, mi(sol['pQY'])
-    
     # The following uses ppl to turn our system of linear inequalities into a 
     # set of extreme points of the corresponding polytope. It then calls 
     # get_solution_val on each extreme point
-    return get_best_solution(cs, get_solution_val)
+    x_opt, v_opt = convex_maximization.maximize_convex_function(
+        f=objective,
+        A_eq=np.array(A_eq, dtype='int'), 
+        b_eq=np.array(b_eq, dtype='int'), 
+        A_ineq=np.array(A_ineq, dtype='int'), 
+        b_ineq=np.array(b_ineq, dtype='int'))
+
+    sol = {}
+    sol['p(Q,Y)'] = x_opt.dot(mul_mx).reshape((n_q,n_y))
+
+    # Compute conditional distributions of Q given each source X_i
+    for rvndx in range(len(pjoint.rvs)-1):
+        pX = pjoint.marginal([rvndx,])
+        cK = 'p(Q|X%d)'%rvndx
+        sol[cK] = np.zeros( (n_q, len(pX.alphabet[0]) ) )
+        for (q,v), k in variablesQgiven[rvndx].items():
+            v_ix = pX._outcomes_index[v]
+            sol[cK][q,v_ix] = x_opt[k]
+
+    # Return mutual information I(Q;Y) and solution information
+    return v_opt, sol
 
