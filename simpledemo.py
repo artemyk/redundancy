17ff373da7ea5eec10288292ff71a81525a1f7cc
Artemy Kolchinsky <artemyk@gmail.com>
2022-03-14 23:37:01 -0600

    simpledemo update

--- simpledemo.py
+++ simpledemo.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 import dit
-import istar
+import blackwell_redundancy 
 
 pjoint = dit.Distribution(['000','010','100','111'], [0.25, 0.25, 0.25, 0.25]) # simple AND gate
-print('Istar:', istar.get_Istar(pjoint)[0])
+print('Iprec:', blackwell_redundancy.get_Iprec(pjoint)[0])
 
